8a4c5e7667a5faef8209cb9c8b8bd3ee10340a92
Daniyal Ashraf <danialashraf15@gmail.com>
2023-01-06 02:45:39 +0500

    UI Initialized
    UI work has been started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+// import ColumnGroupingTable from './Components/ColumnGroupingTable'
+import EnhancedTable from './Components/EnhancedTable'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* <ColumnGroupingTable /> */}
+    <EnhancedTable />
+    </>
+  )
 }
 
-export default App;
+export default App
